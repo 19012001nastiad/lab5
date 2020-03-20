49dd5b96916fe357e3067bf613fb49f65f71b391
Nastia <19yanv2001@gmail.com>
2020-03-20 20:08:22 +0600

    first commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,17 @@
+import data.lesson
+import data.studentList
+import react.dom.h1
+import react.dom.li
+import react.dom.ol
+import react.dom.render
+import kotlin.browser.document
+
+
+fun main() {
+    render(document.getElementById("root")!!) {
+        h1 {
+            +"Students"
+        }
+        lesson()
+    }
+}
