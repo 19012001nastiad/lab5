806b26ea36ef0fde0e5f4fa745ccc1a01472eb80
19012001nastiad <62145610+19012001nastiad@users.noreply.github.com>
2020-03-21 00:33:12 +0600

    Update RStudentList.kt

--- src/main/kotlin/RStudentList.kt
+++ src/main/kotlin/RStudentList.kt
@@ -1,20 +1,29 @@
 import data.Student
-import data.studentList
 import org.w3c.dom.events.Event
 import react.*
 import react.dom.li
-import react.dom.ol
+
 
 interface RStudentListProps : RProps {
     var students: Array<Student>
+    var present: Array<Boolean>
+    var onClick:  (Int) -> (Event) -> Unit
 }
-fun RBuilder.rstudentList(students: Array<Student>,state : Array<Boolean>, onClick: (Int) -> (Event)->Unit) =
-    child(functionalComponent<RStudentListProps> {props ->
-        props.students.mapIndexed {index, student ->
+
+val RFstudentlist =
+    functionalComponent<RStudentListProps> { props ->
+        props.students.mapIndexed { index, student ->
             li {
-                rstudent(student, state[index],onClick(index))
+                rstudent(student, props.present[index], props.onClick(index))
             }
         }
-    }){attrs.students = students }
+    }
+
+fun RBuilder.rstudentlist(students: Array<Student>, present: Array<Boolean>, onClick:(Int) -> (Event) -> Unit) =
+    child(RFstudentlist) {
+        attrs.students = students
+        attrs.present = present
+        attrs.onClick = onClick
+    }
 
 
