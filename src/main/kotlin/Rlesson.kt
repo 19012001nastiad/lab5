48d387824f7b5140365af82f0ce10d9bfd7b47db
19012001nastiad <62145610+19012001nastiad@users.noreply.github.com>
2020-03-21 00:27:35 +0600

    Update Rlesson.kt

--- src/main/kotlin/Rlesson.kt
+++ src/main/kotlin/Rlesson.kt
@@ -1,47 +1,48 @@
-package data
 import data.Student
+import data.Lesson
 import data.studentList
 import org.w3c.dom.events.Event
 import react.*
-import react.dom.h2
 import react.dom.ol
-import rstudentList
 
-interface Rlesson : RProps {
-    var lesson : String
+interface RLessonList : RProps {
+    var subject: Array<Lesson>
     var listStudent :Array<Student>
-}
-
-interface RStudentListState :RState{
     var present: Array<Boolean>
 }
 
-class RStudentlesson : RComponent<Rlesson,RStudentListState>(){
+interface RSubjectState : RState {
+    var present: Array<Boolean>
+}
+class RLesson : RComponent<RLessonList, RSubjectState>() {
     override fun componentWillMount() {
         state.apply {
-            present = Array(props.listStudent.size){false}
+        present = Array(props.listStudent.size) { false }
         }
     }
 
-    override fun RBuilder.render() {
-        h2 {
-            +props.lesson
-        }
-        ol {
-            rstudentList(props.listStudent,state.present, onIndex())
-        }
-    }
-    fun RBuilder.onIndex(): (Int) -> (Event) -> Unit = {
+   fun RBuilder.onIndex(): (Int) -> (Event) -> Unit = {
         onClick(it)
     }
-    fun RBuilder.onClick(index: Int) :(Event) -> Unit = {
-        setState {
-            present[index] = !present[index]
+    override fun RBuilder.render() {
+        props.subject.map {
+            + it.name
+            ol {
+                rstudentlist(props.listStudent, state.present, onIndex())
+            }
+        }
+    }
+
+    fun RBuilder.onClick(index: Int): (Event) -> Unit = {
+      setState {
+         present[index] = !present[index]
         }
     }
 }
-fun RBuilder.lesson()=
-    child(RStudentlesson::class){
-        attrs.lesson = "oop"
+
+    fun RBuilder.rsubject(subject:  ArrayList<Lesson> ) =
+    child( RLesson::class)
+    {
+        attrs.subject = subject.toTypedArray()
         attrs.listStudent = studentList.toTypedArray()
     }
